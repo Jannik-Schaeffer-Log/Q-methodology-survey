ffeec83478ec274e18034bcf61a76d7740b14c9c
Jannik-Schaeffer-Log <118819334+Jannik-Schaeffer-Log@users.noreply.github.com>
2022-11-22 14:16:27 +0100

    Update Q-Methode-code.py

--- Q-Methode-code.py
+++ Q-Methode-code.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Q-Methodology", page_icon="🥑",layout="wide")
 # Read Data
 ########################################
 data_link='https://github.com/Jannik-Schaeffer-Log/Q-methodology-survey/blob/main/data/Policies.csv'
-df_policies= pd.read_csv(data_link, delimiter=';')
+df_policies= pd.read_csv(data_link, sep=';',error_bad_lines=False)
 
 all_policies=df_policies['Policy Component '].unique()
 
